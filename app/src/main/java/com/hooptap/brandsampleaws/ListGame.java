59aa28f9357af5a5a53ed842d47bafad4e279578
carloscarrasco <ccarrasco@hooptap.com>
2016-02-05 12:15:29 +0100

    Subida la app demo para probar el SDK AWS de amazon

--- app/src/main/java/com/hooptap/brandsampleaws/ListGame.java
+++
@@ -1,9 +0,0 @@
-package com.hooptap.brandsampleaws;
-
-import android.support.v7.app.AppCompatActivity;
-
-/**
- * Created by root on 12/01/16.
- */
-public class ListGame  extends AppCompatActivity {
-}
